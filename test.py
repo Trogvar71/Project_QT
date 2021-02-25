e072acc3d8fc143cb29dce6e2b0dd83ab013598b
Trogvar71 <trogvar71@mail.ru>
2021-02-25 10:38:18 +0300

    проверка работоспособности павершел

---
+++ test.py
@@ -0,0 +1,10 @@
+# -*- coding: iso-8859-1 -*-
+import subprocess, sys
+
+
+p = subprocess.Popen(["powershell.exe",
+              "write-output 'test' \n write-output 'test2'"],
+              stdout=sys.stdout)
+
+p.communicate()
+p.kill()
