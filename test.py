310e16b5e7d926a1319ce27bdf0810ee63442a03
Trogvar71 <trogvar71@mail.ru>
2021-03-09 10:44:14 +0300

    появился main

--- test.py
+++ test.py
@@ -5,6 +5,5 @@ import subprocess, sys
 p = subprocess.Popen(["powershell.exe",
               "write-output 'test' \n write-output 'test2'"],
               stdout=sys.stdout)
-
 p.communicate()
 p.kill()
